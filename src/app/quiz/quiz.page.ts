20392dcdd9d5d522f9a489a744ea4787d9bea1d5
Niall-Concannon <nconcannon@gmail.com>
2025-04-20 15:18:52 +0000

    Quiz question feature working, added a bit of css to home

--- src/app/quiz/quiz.page.ts
+++ src/app/quiz/quiz.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonBackButton, IonButtons } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonBackButton, IonButtons, IonButton, IonAlert } from '@ionic/angular/standalone';
 import { QuestionsService } from '../services/questions.service';
 
 @Component({
@@ -9,20 +9,85 @@ import { QuestionsService } from '../services/questions.service';
   templateUrl: './quiz.page.html',
   styleUrls: ['./quiz.page.scss'],
   standalone: true,
-  imports: [IonButtons, IonBackButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonButton, IonButtons, IonBackButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class QuizPage implements OnInit {
 
   myQuestions:any[]=[];
+  randomQ:number = 0;
+  choice:any = '';
+  answer:any = '';
 
   constructor(private questionService:QuestionsService) { }
 
   ngOnInit():void {
     this.questionService.getQuestionsData().subscribe(
       (data)=>{
+        console.log(data);
         this.myQuestions = data;
+        this.randomQ = Math.floor(Math.random() * this.myQuestions.length); // Random generator to choose question
       }
     )
+  }
+
+  checkAnswerA() {
+    this.choice = 'A';
+    this.answer = this.myQuestions[this.randomQ].answer;
+
+    if(this.choice == this.answer) {
+      window.alert("Correct");
+    } else {
+      window.alert("Incorrect");
+    }
+    
+    
+    
+    this.randomQ = Math.floor(Math.random() * this.myQuestions.length);
+  }
+
+  checkAnswerB() {
+    this.choice = 'B';
+    this.answer = this.myQuestions[this.randomQ].answer;
+
+    if(this.choice == this.answer) {
+      window.alert("Correct");
+    } else {
+      window.alert("Incorrect");
+    }
+    
+    
+    
+    this.randomQ = Math.floor(Math.random() * this.myQuestions.length);
+  }
+
+  checkAnswerC() {
+    this.choice = 'C';
+    this.answer = this.myQuestions[this.randomQ].answer;
+
+    if(this.choice == this.answer) {
+      window.alert("Correct");
+    } else {
+      window.alert("Incorrect");
+    }
+    
+    
+    
+    this.randomQ = Math.floor(Math.random() * this.myQuestions.length);
+  }
+
+  checkAnswerD() {
+    this.choice = 'D';
+    this.answer = this.myQuestions[this.randomQ].answer;
+
+    if(this.choice == this.answer) {
+      window.alert("Correct");
+    } else {
+      window.alert("Incorrect");
+    }
+    
+    
+    
+    this.randomQ = Math.floor(Math.random() * this.myQuestions.length);
   }
 
 }
